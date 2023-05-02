d7795d37b1f7ad47bdc35bdf440759904538e0d2
Scomparsa <scomparsa.6k@gmail.com>
2023-05-02 13:48:16 +0800

    refactor(contract): using mapping

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,7 +2,33 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
+    uint public numOfFunders;
+    mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
+
     receive() external payable {}
 
-    function addFunds() external payable {}
+    function addFunds() external payable {
+        address funder = msg.sender;
+
+        if (!funders[funder]) {
+            uint index = numOfFunders++;
+            funders[funder] = true;
+            lutFunders[index] = funder;
+        }
+    }
+
+    function getAllFunders() external view returns (address[] memory) {
+        address[] memory _funders = new address[](numOfFunders);
+
+        for (uint i = 0; i < numOfFunders; i++) {
+            _funders[i] = lutFunders[i];
+        }
+
+        return _funders;
+    }
+
+    function getFundersAtIndex(uint8 index) external view returns (address) {
+        return lutFunders[index];
+    }
 }
