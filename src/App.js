e263c702833dba8daa59781156487968f56bda36
Scomparsa <scomparsa.6k@gmail.com>
2023-05-03 11:38:15 +0800

    feat: basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import "./App.css";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          Current Balance: <strong>10</strong> ETH
+        </div>
+        <button className="btn mr-2">Donate</button>
+        <button className="btn">Withdraw</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
